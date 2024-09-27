3bcdb4d205cdd0966c07fca495731c8a3b7154ab
Adrianna Gmebala <adrianna.gembala@gmail.com>
2024-09-27 08:28:09 +0200

    main changes

--- wild-blog/src/app/app.component.ts
+++ wild-blog/src/app/app.component.ts
@@ -1,21 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ArticleComponent } from './article/article.component';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { CardComponent } from './card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    ArticleComponent
-  ],
+  imports: [RouterOutlet, ArticleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-
 export class AppComponent {
   title = 'Bienvenue sur le Wild Blog de Adrianna';
 };
--- wild-blog/src/app/article/article.component.ts
+++ wild-blog/src/app/article/article.component.ts
@@ -1,22 +1,9 @@
 import { Component } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
-
-interface Card {
-  title: string;
-  description: string;
-  imageUrl: string;
-}
 
 @Component({
   selector: 'app-article',
   standalone: true,
-  imports: [
-
-  ],
+  imports: [],
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.scss'],
 })
