7cd511387934ccd3e2e6ac4879872bdcaa1e6a65
Adrianna Gmebala <adrianna.gembala@gmail.com>
2024-09-27 08:32:25 +0200

    Publier/Dépublier l'article

--- wild-blog/src/app/article/article.component.ts
+++ wild-blog/src/app/article/article.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+interface Article {
+  title: string;
+  author: string;
+  imageUrl: string;
+  content: string;
+  isPublished: boolean;
+}
 
 @Component({
   selector: 'app-article',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.scss'],
 })
-export class ArticleComponent {}
+export class ArticleComponent {
+  article: Article = {
+    title: 'Titre de mon article',
+    author: 'John Doe',
+    imageUrl: 'https://via.placeholder.com/150',
+    content: 'Ceci est le contenu de mon article.',
+    isPublished: true,
+  };
+
+  comment: string = '';
+
+  togglePublish() {
+    this.article.isPublished = !this.article.isPublished;
+  }
+}
