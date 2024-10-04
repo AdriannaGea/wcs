d85eac1fa0cd345cef3b3a82a8b851d6896530de
Adrianna Gmebala <adrianna.gembala@gmail.com>
2024-10-04 15:30:38 +0200

    quete 9.1 - @output

--- wild-blog/src/app/article/article.component.ts
+++ wild-blog/src/app/article/article.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 
 interface Article {
   id: number;
@@ -16,51 +16,17 @@ interface Article {
 @Component({
   selector: 'app-article',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, RouterModule],
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.scss'],
 })
 export class ArticleComponent {
-  articleId: number | undefined;
-  article: Article | undefined;
+  @Input() article: Article | undefined;
+  @Output() likeEvent = new EventEmitter<Article>();
 
-  articles: Article[] = [
-    {
-      id: 1,
-      title: 'Angular 16: Les nouveautés',
-      author: 'Alice',
-      content: "Les nouveautés d'Angular 16 incluent...",
-      imageUrl: 'https://via.placeholder.com/350x150',
-      isPublished: true,
-      likes: 120,
-    },
-    {
-      id: 2,
-      title: 'Développer une API REST',
-      author: 'Bob',
-      content: 'Développer une API REST nécessite...',
-      imageUrl: 'https://via.placeholder.com/350x150',
-      isPublished: false,
-      likes: 75,
-    },
-    {
-      id: 3,
-      title: 'Pourquoi TypeScript est essentiel ?',
-      author: 'Charlie',
-      content: 'TypeScript apporte de la robustesse...',
-      imageUrl: 'https://via.placeholder.com/350x150',
-      isPublished: true,
-      likes: 200,
-    },
-  ];
-
-  constructor(private route: ActivatedRoute, private router: Router) {
-    this.articleId = Number(this.route.snapshot.paramMap.get('id'));
-    this.article = this.articles.find(
-      (article) => article.id === this.articleId
-    );
-    if (!this.article) {
-      this.router.navigate(['/not-found']);
+  onLike() {
+    if (this.article) {
+      this.likeEvent.emit(this.article);
     }
   }
 }
